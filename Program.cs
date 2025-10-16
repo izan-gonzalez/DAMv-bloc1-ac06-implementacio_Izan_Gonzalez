a44fced0f7067ace78f4476e72501df535977830
Izan <izan.gonzalez.7e8@itb.cat>
2025-10-16 12:45:59 +0200

    Correct of ejercice 1

--- Program.cs
+++ Program.cs
@@ -1,13 +1,25 @@
 ﻿using System;
-using System.Numerics;
-namespace HelloWorldProject
+namespace Project
 {
     public class HelloWorld
     {
         public static void Main()
         {
-            Console.Write("HelloWorld, World");
+            const string InPut1 = "Enter the first integer: ";
+            const string InPut2 = "Enter the second integer: ";
+            const string Result = "The sum is: ";
+
+            int Num1, Num2, Sum;
+
+            Console.Write(InPut1);
+            Num1=int.Parse(Console.ReadLine());
+
+
+            Console.Write(InPut2);
+            Num2 = int.Parse(Console.ReadLine());
+
+            Sum=Num1 + Num2;
+            Console.Write(Result+Sum);
         }
     }
 }
-
