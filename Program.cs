56b0b618d8aaed87645ea98af4ce8d1d6082fa29
Izan <izan.gonzalez.7e8@itb.cat>
2025-10-16 12:11:40 +0200

    I created  HelloWorld program

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Numerics;
+namespace HelloWorldProject
+{
+    public class HelloWorld
+    {
+        public static void Main()
+        {
+            Console.Write("HelloWorld, World");
+        }
+    }
+}
+
