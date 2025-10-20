4082bc1b228d6f88c16836adae5c9b0c5aab301c
Izan <izan.gonzalez.7e8@itb.cat>
2025-10-21 01:37:24 +0200

    Pseudo codigo ex2

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Project
+{
+    public class HelloWorld
+    {
+        public static void Main()
+        {
+            const string Input1 = "Enter temperature in Celsius: ";
+            const string Result = "Temperature in Kelvin: ";
+
+            double Celsius, Kelvin;
+
+            Console.Write(Input1);
+            Celsius= double.Parse(Console.ReadLine());
+            Kelvin = Celsius + 273.15;
+
+            Console.Write(Result+Kelvin);
+        }
+    }
+}
