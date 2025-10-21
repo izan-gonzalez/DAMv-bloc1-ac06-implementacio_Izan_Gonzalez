f969d75915009f528d60951222d0b0612d9c6c6d
Izan <izan.gonzalez.7e8@itb.cat>
2025-10-21 02:35:27 +0200

    Pseudo codi ex5

--- Program.cs
+++ Program.cs
@@ -1,24 +1,32 @@
 ﻿using System;
+using System.Diagnostics.Metrics;
 using System.Runtime.InteropServices;
 using System.Security.Authentication;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace Project
 {
     public class Euros_to_Dolars
     {
         public static void Main()
         {
-            const double Exchange = 1.12;
-            const string Input= "Enter amount in euros: ";
-            const string Result = "Amount in dollars: ";
+            const string Input1= "Enter the first number: ";
+            const string Input2= "Enter the second number: ";
+            const string Input3= "Enter the third number: ";
+            const string Result = "The average is: ";
 
-            double Euros, Dollars;
+            int Num1, Num2, Num3, Average;
 
-            Console.Write(Input);
-            Euros= double.Parse(Console.ReadLine());
+            Console.Write(Input1);
+            Num1= int.Parse(Console.ReadLine());
 
-            Dollars = Euros * Exchange;
+            Console.Write(Input2);
+            Num2 = int.Parse(Console.ReadLine());
 
-            Console.Write(Result+Dollars);
+            Console.Write(Input3);
+            Num3 = int.Parse(Console.ReadLine());
+
+            Average = (Num1 + Num2 + Num3) / 3;
+            Console.Write(Result+Average);
         }
     }
 }
