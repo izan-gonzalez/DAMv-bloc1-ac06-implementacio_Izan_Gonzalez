013a97116e458156d2a72eb7f5facd0b465ff75e
Izan <izan.gonzalez.7e8@itb.cat>
2025-10-23 04:54:23 +0200

    ex3 pseudocodi

--- Program.cs
+++ Program.cs
@@ -1,27 +1,50 @@
 ﻿using System;
+using System.Diagnostics.Metrics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace Project
 {
-    public class Is_Natural
+    public class NumberWithAttempts
     {
         public static void Main()
         {
-            const string Input = "Exter a value to check if it's within interval: ";
-            const string Ok = "is within the interval (5, 50)";
-            const string Ko = "is NOT whithin the interval";
+            const string Input ="Enter an integer number within the interval(2, 30):";
+            const string ATTEMPTS = "Attempt of 3:";
+            const string Ok = "Valid number entered: ";
+            const string Ko = "Error: The number is not within the interval (2, 30). It must be between 3 and 29 (inclusive).";
+            const string Lost= "You have lost. No valid number entered.";
 
-            int Num;
+            int Num,attempts;
+            bool IsValid;
 
-            Console.Write(Input);
 
-            Num = int.Parse(Console.ReadLine());
+            attempts = 1;
+            IsValid=false;
 
-            if ((Num > 5) && (Num < 50))
+            while (attempts <= 3 && !IsValid)
             {
-                Console.Write(Ok);
+                Console.WriteLine(ATTEMPTS + attempts);
+                Console.Write(Input);
+                Num = int.Parse(Console.ReadLine());
+
+
+                if (Num > 2 && Num < 30)
+                {
+                    IsValid = true;
+                }
+                else
+                {
+                    IsValid = false;
+                    attempts++;
+                }
+            }
+
+            if (IsValid)
+            {
+                Console.WriteLine(Ok);
             }
             else
             {
-                Console.Write(Ko);
+                Console.WriteLine(Ko);
             }
         }
     }
